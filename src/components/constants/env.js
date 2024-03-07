2bc7a328cc8ce050dfe342a2146efbca49ba2a4c
sikderemran <sikderemran4688@gmail.com>
2024-03-07 23:50:13 +0600

    endpoint chnaged

--- src/components/constants/env.js
+++ src/components/constants/env.js
@@ -1,10 +1,10 @@
 module.exports = global.config = {
     data: {
         path:{
-            // basePath:'https://emransikder.pythonanywhere.com/api',
-            // filePath:'https://emransikder.pythonanywhere.com/',
-            basePath:'http://127.0.0.1:8000/api',
-            filePath:'http://127.0.0.1:8000/'
+            basePath:'https://emransikder.pythonanywhere.com/api',
+            filePath:'https://emransikder.pythonanywhere.com/',
+            // basePath:'http://127.0.0.1:8000/api',
+            // filePath:'http://127.0.0.1:8000/'
         },
       
     }
